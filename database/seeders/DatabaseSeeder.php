c574dcb0623e06954e3bab98eb98b54ab08d9de9
Nathanael Ferry Pratama <ntnferry1505@gmail.com>
2024-02-20 01:00:17 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            // SeedersProductSeeder::class,
+            SeedersProductSeeder::class,
             WebProfileSeeder::class,
         ]);
     }
